d8c8937e750f162af19837ff2df54aad9b3365bd
anhtn <tongngocanhcampha@gmail.com>
2022-06-24 17:20:05 +0700

    Login by Authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import React from 'react'
-
-/* スタイルシート */
+import firebase from 'firebase/compat/app';
+import React, { useEffect, useState } from 'react'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import Todo from './components/Todo';
 import './styles/main.css';
+/* スタイルシート */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDE0ZRKjIjWUn9JfCcT-WGjOYJEgFpINXM",
+  authDomain: "itss-training-firebase.firebaseapp.com",
+  projectId: "itss-training-firebase",
+  storageBucket: "itss-training-firebase.appspot.com",
+  messagingSenderId: "355192140055",
+  appId: "1:355192140055:web:a860f5dbec305f68e9c752"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
+};
 
 /* コンポーネント */
-import Todo from './components/Todo';
 
 function App() {
+
+  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+    });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      </div>
+    );
+  }
+
   return (
     <div className="container is-fluid">
+      <p>{firebase.auth().currentUser.displayName}</p>
+      <a onClick={() => firebase.auth().signOut()}>Logout</a>
       <Todo />
     </div>
   );
